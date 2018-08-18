145020586d8ffe6dba60ff01554b35e2b135f827
sitalobr <simaoitalo13@gmail.com>
2018-08-17 21:08:04 -0300

    initial commit

---
+++ app-sorteio/app/src/main/java/com/sitalobr/dev/appsorteio/MainActivity.java
@@ -0,0 +1,24 @@
+package com.sitalobr.dev.appsorteio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Random random = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero(View view) {
+        TextView txtNumber = findViewById(R.id.txtNumber);
+        txtNumber.setText("" + random.nextInt(100));
+    }
+}
