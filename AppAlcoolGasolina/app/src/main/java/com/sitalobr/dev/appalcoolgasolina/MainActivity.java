e493a762d3e5357040d662209be77fc50c8c2768
sitalobr <simaoitalo13@gmail.com>
2018-08-30 00:31:23 -0300

    removing unused imports

--- AppAlcoolGasolina/app/src/main/java/com/sitalobr/dev/appalcoolgasolina/MainActivity.java
+++ AppAlcoolGasolina/app/src/main/java/com/sitalobr/dev/appalcoolgasolina/MainActivity.java
@@ -1,6 +1,5 @@
 package com.sitalobr.dev.appalcoolgasolina;
 
-import android.graphics.Color;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
